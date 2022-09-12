1711615f4a7a4d5a61f3b804e7e34208eb27d262
AiperiAkimova <aiperi.peaksoft@gmail.com>
2022-09-13 03:03:56 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import "./App.css";
 import Login from "./components/pages/Login";
-import {Routes, Route} from 'react-router-dom'
-import HomePage from './components/pages/HomePage'
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
 import Trello from "./components/pages/Trello";
 
-
 function App() {
-	
   return (
-    
     <div className="App">
-      
       <Routes>
         <Route path="/" element={<HomePage />} />
-      <Route path="/login" element = {<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/trello" element={<Trello />} />
-        
       </Routes>
-     
     </div>
   );
 }
 
 export default App;
-
